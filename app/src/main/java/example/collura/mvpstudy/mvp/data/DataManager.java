dc6d61b3042aa2ece4f8d4db12a19e07a7287587
Roberto Xavier Collura <collura.developer@gmail.com>
2018-04-16 20:11:08 -0300

    Update DataManager.java

--- app/src/main/java/example/collura/mvpstudy/mvp/data/DataManager.java
+++ app/src/main/java/example/collura/mvpstudy/mvp/data/DataManager.java
@@ -1,7 +1,7 @@
 package example.collura.mvpstudy.mvp.data;
 
 
-// IMPLEMENTA TODAS AS INTERFACES HELPER HERDANDO TODOS OS METODOS
+// HERDA TODAS AS INTERFACES HELPER HERDANDO TODOS OS METODOS
 interface DataManager
     extends  NetworkHelper,DbHelper, PreferencesHelper{
 
